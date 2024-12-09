717e0b2b9a9d51153efdbd287b335ae584b2bbe6
Rafael Santana <88090582+rafa-san@users.noreply.github.com>
2024-12-09 16:41:49 -0300

    style: ajustes na indentação

--- pyramid-generator.js
+++ pyramid-generator.js
@@ -7,25 +7,6 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-
-/*
-#1
-for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}
-*/
-
-
-/*
-#2
-while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}
-*/
-
-
-//#3
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
